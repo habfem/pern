13b598a75845d47ae3176520bdd51dafeb5ec697
habfem <habeebmustapha96@gmail.com>
2023-03-13 23:09:42 -0500

    map

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -91,7 +91,8 @@ const Slider = () => {
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
-          <Slide bg={item.bg}>
+          <Slide bg={item.bg} key={item
+            .id}>
             <ImgContainer>
               <Image src={item.img} />
             </ImgContainer>
