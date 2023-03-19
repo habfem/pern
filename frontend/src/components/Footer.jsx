410e0782e34fcac914fe98e6e93bea62c1517442
habfem <habeebmustapha96@gmail.com>
2023-03-18 21:43:31 -0500

    mobile

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons"
 import styled from "styled-components"
+import { mobile } from "../responsive"
 
 const Container = styled.div`
   display: flex;
+  ${mobile({ flexDirection: "column" })};
 `
 const Left = styled.div`
   flex: 1;
@@ -33,6 +35,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ dsiplay: "none" })};
 `
 const Title = styled.h3`
   margin-bottom: 30px;
@@ -50,6 +53,8 @@ const ListItem = styled.li`
 `
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })};
 `
 const ContactItem = styled.div`
   margin-bottom: 20px;
---
+++ frontend/src/pages/Product.jsx
@@ -0,0 +1,156 @@
+import styled from "styled-components"
+import Navbar from "../components/Navbar"
+import Announcement from "../components/Announcement"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
+import { Add, Remove } from "@material-ui/icons"
+import { mobile } from "../responsive"
+
+const Container = styled.div`
+  padding: 0px;
+`
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  ${mobile({ padding: "10px", flexDirection: "column" })};
+`
+const ImageContainer = styled.div`
+  flex: 1;
+`
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+  ${mobile({ height: "40vh" })};
+`
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+  ${mobile({ padding: "10px" })};
+
+`
+const Title = styled.h1`
+  font-weight: 200;
+`
+const Desc = styled.p`
+  margin: 20px 0px;
+`
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px; 
+`
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ width: "100%" })};
+`
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${props => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`
+const FilterSizeOption = styled.option`
+  padding: 0px;
+`
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ width: "100%" })};
+`
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 0.5px solid blue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`
+const Button = styled.button`
+  padding: 15px;
+  border: 1.5px solid blue;
+  border-radius: 10px;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover{
+    background-color: #f8f7f3;
+  }
+`
+
+const Product = () => {
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Wrapper>
+        <ImageContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImageContainer>
+        <InfoContainer>
+          <Title>Denim Jump Suit</Title>
+          <Desc></Desc>
+          <Desc>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In architecto ducimus ipsam eveniet laudantium exercitationem officiis odit. Laboriosam quos quia voluptatem error! Dolorem sit consectetur inventore, in aliquid doloribus iusto.</Desc>
+          <Price>₦ 25,000</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+                <FilterSizeOption>XXL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  )
+}
+
+export default Product
