6ad7fe8af1e58393224b1cb16ef5828a5c99530c
habfem <habeebmustapha96@gmail.com>
2023-03-22 03:10:28 -0500

    token

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,24 @@
-import express from "express"
+import express from "express";
+import { verifyToken, verifyTokenAndAuthorization } from "./verifyToken.js";
+import User from "../models/UserModel.js";
 
-const router = express.Router()
+const router = express.Router();
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body
+    },
+      { new: true }
+    )
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 export default router
